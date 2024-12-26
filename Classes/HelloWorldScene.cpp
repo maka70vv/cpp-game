a97b47803491aa9b5f0bc89143951a9c298a3559
maka70vv <25.makarovv@gmail.com>
2024-12-26 20:09:08 +0600

    ready for exam

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,33 +1,19 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "HelloWorldScene.h"
 #include <iostream>
 
+#include "CCDirector.h"
+#include "CCEventListenerMouse.h"
+#include "CCPlatformMacros.h"
+#include "2d/CCSprite.h"
+
+namespace cocos2d
+{
+    class Scene;
+}
+
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+auto HelloWorld::createScene() -> cocos2d::Scene*
 {
     return HelloWorld::create();
 }
@@ -53,84 +39,223 @@ bool HelloWorld::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     shipPtr = Sprite::create("ship.png");
+    GameSingleton::getInstance().getState().shipPos = Vec2(300, 300);
+    GameSingleton::getInstance().getState().bkgPos = Vec2(1325, 400);
 
-    GameSingleton::getInstance().getState().reset();
-   
-    
-    this->addChild(shipPtr, 2);
 
+    shipPtr->setPosition(300, 300);
+    this->addChild(shipPtr,2);
     mouseListener = EventListenerMouse::create();
-
-    mouseListener->onMouseMove 
+    mouseListener->onMouseMove
         = CC_CALLBACK_1(HelloWorld::onMouseMove, this);
     mouseListener->onMouseDown
         = CC_CALLBACK_1(HelloWorld::onMouseDown, this);
-
     _eventDispatcher->
         addEventListenerWithSceneGraphPriority(mouseListener, this);
 
+    auto keyListener = EventListenerKeyboard::create();
+    keyListener->onKeyPressed = CC_CALLBACK_2(HelloWorld::onKeyPressed, this);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(keyListener, this);
     scheduleUpdate();
 
     backgroundPtr = Sprite::create("bkg.png");
+       backgroundPtr ->setPosition(0, 0);
+    this->addChild(backgroundPtr,1);
 
-    this->addChild(backgroundPtr, 1);
+    scheduleUpdate();
 
-    SetAllPositions();
-
+    GameSingleton::getInstance().getState().reset();
+    proxy.factory = &factory;
     return true;
 }
-
 void HelloWorld::onMouseMove(cocos2d::Event* event)
 {
     cocos2d::EventMouse* mouseEvent = (cocos2d::EventMouse*)event;
-
     GameSingleton::getInstance().getState().shipPos.y = mouseEvent->getCursorY();
+        SetALLPositions();
 
-    SetAllPositions();
+
 }
-
 void HelloWorld::onMouseDown(cocos2d::Event* event)
 {
     GameSingleton::getInstance().getState().reset();
-    SetAllPositions();
+    SetALLPositions();
 }
-
-void HelloWorld::update(float delta)
-{
-    GameSingleton::getInstance().getState().moveBkg(80 * delta);
-    GameSingleton::getInstance().getState().moveObjectPos(200 * delta);
-
-    SetAllPositions();
-
-    objectTimer += delta;
-
-    if (objectTimer > 2.0)
-    {
-        objectTimer = objectTimer - 2.0;
-
-        Vec2 pos = Vec2{ 1400.f, (float)(rand() % 500)};
-
-        // Create asteroid here
-        GameSingleton::getInstance()
-            .getState()
-            .objectPos.push_back(pos);
-
-        Sprite* newObject = Sprite::create("asteroid.png");
-        newObject->setPosition(pos);
-        addChild(newObject, 3);
-        objectPtr.push_back(newObject);
-    }
-}
-
-void HelloWorld::SetAllPositions()
+void HelloWorld::SetALLPositions()
 {
     shipPtr->setPosition(GameSingleton::getInstance().getState().shipPos);
     backgroundPtr->setPosition(GameSingleton::getInstance().getState().bkgPos);
-
     for (int i = 0; i < objectPtr.size(); i++)
     {
         auto position = GameSingleton::getInstance().getState().objectPos[i];
-        objectPtr[i]->setPosition(position);
+        objectPtr[i]->setPosition(position.pos);
     }
 }
 
+ObjectData  ObjectFactory::createObject()
+{
+    ObjectData result;
+    Vec2 pos = Vec2{ 1400.f, (float)(rand() % 500) };
+    int type = rand() % 2;
+    std::string name;
+    if (type == 0)
+    {
+        name = "asteroid.png";
+    }
+    else
+    {
+        name = "cloud.png";
+    }
+    result.pos = pos;
+    Sprite* newObject = Sprite::create(name);
+    newObject->setScale(scale);
+    newObject->setPosition(pos);
+    result.sprite = newObject;
+    scale = scale * 1.15;
+    result.type = type;
+    return result;
+
+
+
+}
+void HelloWorld::onKeyPressed(EventKeyboard::KeyCode code, Event* event)
+{
+    if (code == EventKeyboard::KeyCode::KEY_S)
+    {
+       // MessageBoxA(0, "Key S pressed", "Key S presd", 0);
+        Memento save = GameSingleton::getInstance().getState().SaveData();
+        saves.push_back(save);
+
+    }
+    else if (code == EventKeyboard::KeyCode::KEY_L)
+    {
+        //MessageBoxA(0, "Key L pressed", "Key L presd", 0);
+        if (saves.size() > 0)
+        {
+            Memento save = saves.back();
+            GameSingleton::getInstance().getState().RestoreFromData(save);
+            reload();
+        }
+
+    }
+}
+void HelloWorld::update(float delta)
+{
+    GameSingleton::getInstance().getState().moveBkg(80*delta);
+    GameSingleton::getInstance().getState().moveObkectPos(200 * delta);
+    SetALLPositions();
+    GameSingleton::getInstance().getState().objectTimer += delta;
+    if (GameSingleton::getInstance().getState().objectTimer > 2.0)
+    {
+        GameSingleton::getInstance().getState().objectTimer = GameSingleton::getInstance().getState().objectTimer - 2.0;
+
+        auto newobject = proxy.createObject();
+
+        std::shared_ptr<Strategy> strategy;
+        int type = rand() % 2;
+        if (type == 0)
+        {
+            strategy = std::make_shared<Strategy>();
+
+        }
+        else
+        {
+            strategy = std::make_shared<ZigzagStrategy>();
+        }
+        GameSingleton::getInstance()
+            .getState()
+            .objectPos.push_back({ newobject.pos,strategy, type, newobject.sprite->getScale(), newobject.type
+
+                });
+
+        addChild(newobject.sprite, 3);
+        objectPtr.push_back(std::make_unique<ShakingSpriteDecorator>(newobject.sprite,newobject.type ));
+    }
+    for (auto& object : objectPtr)
+    {
+        float x = object->getPosition().x;
+        float y = object->getPosition().y;
+        float R = object->getScale() * 300;
+        float w = 150;
+        float h = 150;
+        float px = shipPtr->getPosition().x;
+        float py = shipPtr->getPosition().y;
+
+        if ((abs(y - (py - h / 2)) < R
+            || abs(y + (py - h / 2)) < R)
+            && (x > px - w / 2)
+            && (x < px + w / 2))
+        {
+            auto& flyingObject = object->sprite;
+            flyingObject->accept(*this);
+        }
+
+
+   }
+
+
+}
+
+void HelloWorld::reload()
+{
+    for (auto& object : objectPtr)
+    {
+        object->sprite->sprite->removeFromParent();
+    }
+    objectPtr.clear();
+    for (auto& ObjectPosAndStrategy : GameSingleton::getInstance().getState().objectPos)
+    {
+        ObjectData newObject;
+        newObject.pos = ObjectPosAndStrategy.pos;
+        if (ObjectPosAndStrategy.objectType == 0) {
+            newObject.sprite = Sprite::create("asteroid.png");
+        }
+        else {
+            newObject.sprite = Sprite::create("cloud.png");
+
+        }
+        newObject.type = ObjectPosAndStrategy.objectType;
+        newObject.sprite->setPosition(ObjectPosAndStrategy.pos);
+        newObject.sprite->setScale(ObjectPosAndStrategy.scale);
+
+        addChild(newObject.sprite, 3);
+        objectPtr
+            .push_back(
+                std::make_unique<ShakingSpriteDecorator>(newObject.sprite, newObject.type)
+            );
+
+    }
+}
+
+
+ObjectData ObjectProxy::createObject()
+{
+    auto& objectCreationData = GameSingleton::getInstance().getState().objectCraetionData;
+
+    if (objectCreationData.size() > 0)
+    {
+        ObjectCreationData d = objectCreationData[objectCreationData.size() - 1];
+        objectCreationData.pop_back();
+        ObjectData result;
+        result.pos = d.pos;
+        if (d.type == 0)
+        {
+            result.sprite = Sprite::create("asteroid.png");
+        }
+        else
+        {
+
+            result.sprite = Sprite::create("cloud.png");
+        }
+        result.type = d.type;
+        result.sprite->setPosition(d.pos);
+        result.sprite->setScale(d.scale);
+        return result;
+    }
+    else
+    {
+        return factory->createObject();
+    }
+
+
+}
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,112 +1,355 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
 
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef HELLOWORLD_SCENE_H
+#define HELLOWORLD_SCENE_H
+
+#include <iostream>
 
 #include "cocos2d.h"
 #include <vector>
-
 using namespace cocos2d;
 
-class GameState
+struct ObjectCreationData
+{
+    double scale = 1.0;
+    cocos2d::Vec2 pos;
+    int type = 0;
+
+};
+
+class Strategy
 {
 public:
-    cocos2d::Vec2 shipPos;
-    cocos2d::Vec2 bkgPos;
-    std::vector<cocos2d::Vec2> objectPos;
-
-
-    void reset()
+    virtual void changePosition(cocos2d::Vec2& pos, float x)
     {
-        shipPos = cocos2d::Vec2(300, 300);
-        bkgPos = cocos2d::Vec2(1325, 400);
+        pos.x = pos.x - x;
     }
-
-    void moveBkg(float x)
+};
+class ZigzagStrategy : public Strategy
+{
+public:
+    float shift = 0;
+    float dir = -1;
+    void changePosition(cocos2d::Vec2& pos, float x)
     {
-        bkgPos.x = bkgPos.x - x;
-        if (bkgPos.x < 0)
+        pos.x = pos.x - x;
+        pos.y = pos.y + dir * x;
+        shift = shift + dir * x;
+        if (shift > 100)
         {
-            bkgPos.x += 1325;
+            dir = -1;
         }
+        if (shift < -100)
+        {
+            dir = 1;
+        }
+
     }
 
-    void moveObjectPos(float x)
+};
+
+struct ObjectPosAndStrategyData
+{
+    cocos2d::Vec2 pos;
+    int strategy;
+    float scale;
+    int objectType;
+};
+
+struct ObjectPosAndStrategy
+{
+    cocos2d::Vec2 pos;
+    std::shared_ptr<Strategy> strategy;
+    int strategyType;
+    float scale;
+    int objectType;
+    void move(float x)
     {
-        for (auto& obj : objectPos)
+        strategy -> changePosition(pos,x);
+    }
+    ObjectPosAndStrategyData SaveData()
+    {
+        ObjectPosAndStrategyData result;
+        result.pos = pos;
+        result.strategy = strategyType;
+        result.scale = scale;
+        result.objectType = objectType;
+        return result;
+
+    }
+    void RestoreFromData(ObjectPosAndStrategyData data)
+    {
+        pos = data.pos;
+        strategyType = data.strategy;
+        scale = data.scale;
+        objectType = data.objectType;
+        if (strategyType == 0)
         {
-            obj.x = obj.x - x;
+            strategy = std::make_shared<Strategy>();
+        }
+        else
+        {
+            strategy = std::make_shared<ZigzagStrategy>();
         }
     }
 };
 
+
+
+struct  Memento
+{
+    double objectTimer = 0;
+    cocos2d::Vec2 shipPos;
+    cocos2d::Vec2 bkgPos;
+    std::vector<ObjectPosAndStrategyData> objectPosAndStrategyData;
+    std::vector<ObjectCreationData> objectCreationData;
+};
+
+
+class GameState
+{
+protected:
+
+public:
+    double objectTimer = 0;
+    cocos2d::Vec2 shipPos;
+    cocos2d::Vec2 bkgPos;
+    std::vector<ObjectPosAndStrategy> objectPos;
+    std::vector<ObjectCreationData> objectCraetionData;
+
+    Memento SaveData()
+    {
+        Memento result;
+        result.objectTimer = objectTimer;
+        result.shipPos = shipPos;
+        result.bkgPos = bkgPos;
+        result.objectCreationData = objectCraetionData;
+
+        for (auto& object : objectPos)
+        {
+            result.objectPosAndStrategyData.push_back(object.SaveData());
+        }
+        return result;
+    }
+    void RestoreFromData(Memento data)
+    {
+        objectTimer = data.objectTimer;
+        shipPos = data.shipPos;
+        bkgPos = data.bkgPos;
+        objectCraetionData = data.objectCreationData;
+        objectPos.clear();
+        for (auto& objectAndStrategy : data.objectPosAndStrategyData)
+        {
+            ObjectPosAndStrategy obj;
+            obj.RestoreFromData(objectAndStrategy);
+            objectPos.push_back(obj);
+        }
+    }
+    void reset()
+    {
+        shipPos = cocos2d::Vec2(300, 300);
+        bkgPos = cocos2d::Vec2(1325, 400);
+
+        objectCraetionData.push_back({ 0.2,{1400,300}, 0 });
+        objectCraetionData.push_back({ 0.2,{1400,300}, 0 });
+        objectCraetionData.push_back({ 0.2,{1400,300}, 0 });
+        objectCraetionData.push_back({ 0.2,{1400,300}, 0 });
+        objectCraetionData.push_back({ 0.2,{1400,300}, 0 });
+
+
+    }
+    void moveBkg(float x)
+    {
+        bkgPos.x = bkgPos.x - x;
+        if (bkgPos.x < -0)
+        {
+            bkgPos.x += 1325;
+        }
+
+    }
+    void moveObkectPos(float x)
+    {
+        for (auto& obj : objectPos)
+        {
+            obj.move(x);
+        }
+    }
+
+};
 class GameSingleton
 {
 protected:
-    GameSingleton() {}
-
+    GameSingleton(){}
     GameState currentState;
 public:
-    static GameSingleton& getInstance()
+   static GameSingleton& getInstance()
     {
         static GameSingleton instance;
-
         return instance;
     }
-
     GameState& getState()
     {
         return currentState;
     }
 };
 
+struct ObjectData
+{
+    cocos2d::Vec2 pos;
+    cocos2d::Sprite* sprite;
+    int type;
+    ObjectData& setPos(cocos2d::Vec2 newpos)
+    {
+        pos = newpos;
+        return *this;
+
+    }
+    ObjectData& setScale(float s)
+    {
+        sprite->setScale(s);
+        return *this;
+    }
+};
+
+class ObjectSourceInterface
+{
+public:
+    virtual ObjectData createObject() = 0;
+};
+class ObjectFactory : public ObjectSourceInterface
+{
+public:
+    double scale = 0.1;
+    ObjectData createObject();
+};
+class ObjectProxy : public ObjectSourceInterface
+{
+public:
+    ObjectData createObject();
+    ObjectFactory* factory = nullptr;
+
+};
+struct SolidObject;
+struct CloudObject;
+
+struct Visitor
+{
+    virtual void visitObject(SolidObject& s)
+    {
+        std::cout << "Solid Object" << std::endl;
+    }
+    virtual void visitObject(CloudObject& s)
+    {
+        std::cout << "Cloud Object" << std::endl;
+    }
+};
+
+
+
+
+struct FlyingObject
+{
+    cocos2d::Sprite* sprite;
+    FlyingObject(cocos2d::Sprite* s)
+        :sprite{s}
+    {}
+    virtual void accept(Visitor& v) = 0;
+
+
+};
+
+struct SolidObject : public FlyingObject
+{
+
+    SolidObject(cocos2d::Sprite* s)
+        :FlyingObject{ s }
+    {}
+    virtual void accept(Visitor& v)
+    {
+        v.visitObject(*this);
+    }
+};
+
+struct CloudObject : public FlyingObject
+{
+    CloudObject(cocos2d::Sprite* s)
+        :FlyingObject{ s }
+    {}
+    virtual void accept(Visitor& v)
+    {
+        v.visitObject(*this);
+    }
+};
+
+class SpriteDecorator
+{
+public:
+    //cocos2d::Sprite* sprite;
+    std::unique_ptr<FlyingObject> sprite;
+public:
+    SpriteDecorator(cocos2d::Sprite* s, int type)
+    {
+        if (type == 0)
+        {
+            sprite = std::unique_ptr<FlyingObject>{ new SolidObject{s} };
+        }
+        else
+        {
+            sprite = std::unique_ptr<FlyingObject>{ new CloudObject{s} };
+        }
+    }
+    virtual void setPosition(cocos2d::Vec2 p)
+    {
+        sprite->sprite->setPosition(p);
+    }
+    virtual cocos2d::Vec2 getPosition()
+
+    {
+        return sprite->sprite->getPosition();
+
+    }
+    virtual float getScale()
+    {
+        return sprite->sprite->getScale();
+    }
+};
+class ShakingSpriteDecorator : public SpriteDecorator
+{
+public:
+    ShakingSpriteDecorator(cocos2d::Sprite* s, int type)
+        :SpriteDecorator(s, type)
+    {
+
+    }
+    void setPosition(cocos2d::Vec2 p)override
+    {
+        p = p + cocos2d::Vec2{ rand() % 10 - 5.f,rand() % 10 - 5.f };
+        SpriteDecorator::setPosition(p);
+    }
+};
+
 
 class HelloWorld : public cocos2d::Scene
+    ,public Visitor
 {
 public:
     static cocos2d::Scene* createScene();
-
     virtual bool init();
-
     cocos2d::Sprite* shipPtr;
     cocos2d::Sprite* backgroundPtr;
-    std::vector<cocos2d::Sprite*> objectPtr;
 
-    double objectTimer = 0;
-
+    std::vector<std::unique_ptr<SpriteDecorator>> objectPtr;
+    std::vector<Memento> saves;
+    ObjectFactory factory;
+    ObjectProxy proxy;
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-
     cocos2d::EventListenerMouse* mouseListener;
-
     void onMouseMove(cocos2d::Event* event);
     void onMouseDown(cocos2d::Event* event);
-
     void update(float delta) override;
-
-    void SetAllPositions();
+    void onKeyPressed(EventKeyboard::KeyCode code, Event* event);
+    void SetALLPositions();
+    void reload();
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif // HELLOWORLD_SCENE_H
